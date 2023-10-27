d6841b677cbef385857a30c52948e65383e7f0ea
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-10-27 11:06:10 +0100

    Update: Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This script will create a file in the tmp directory
+file { '/tmp/school':
+  ensure => present,
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
+  content => 'I love Puppet',
+}
