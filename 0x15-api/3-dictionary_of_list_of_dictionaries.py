d65506cfdce1e6861705d13e0854b0488ecf803e
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-11-27 19:35:21 +0100

    Update: 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,14 +5,14 @@ import json
 import requests
 
 def export_all_employees_data_to_json():
-	# Fetch all users and their respective TODO items
+	""" Fetch all users and their respective TODO items """
 	users_response = requests.get("https://jsonplaceholder.typicode.com/users")
 	users_data = users_response.json()
 
 	todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 	todos_data = todos_response.json()
 
-	# Create and populate JSON data structure
+	""" Create and populate JSON data structure """
 	all_employees_data = {}
 
 	for user in users_data:
@@ -31,7 +31,7 @@ def export_all_employees_data_to_json():
 
 		all_employees_data[str(employee_id)] = task_list
 
-	# Create JSON file and write data
+	""" Create JSON file and write data """
 	filename = 'todo_all_employees.json'
 	with open(filename, mode='w') as f:
 		json.dump(all_employees_data, f, indent=4)
