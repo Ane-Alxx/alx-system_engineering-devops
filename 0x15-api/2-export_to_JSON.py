5990b67b07e02f6a5c0778e4ac712a17d268d758
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-11-27 19:33:00 +0100

    Update: 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,16 +5,16 @@ import json
 import requests
 
 def export_employee_data_to_json(employee_id):
-	# Fetch user information
+	""" Fetch user information """
 	user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
 	user_data = user_response.json()
 	username = user_data.get('username')
 
-	# Fetch TODO items for the specified employee
+	""" Fetch TODO items for the specified employee """
 	todos_response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id))
 	todos_data = todos_response.json()
 
-	# Create and populate JSON data structure
+	""" Create and populate JSON data structure """
 	employee_data = {}
 	task_list = []
 
@@ -28,7 +28,7 @@ def export_employee_data_to_json(employee_id):
 
 	employee_data[str(employee_id)] = task_list
 
-	# Create JSON file named after the user ID and write data
+	""" Create JSON file named after the user ID and write data """
 	filename = str(employee_id) + '.json'
 	with open(filename, mode='w') as f:
 		json.dump(employee_data, f, indent=4)
